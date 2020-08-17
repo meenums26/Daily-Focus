dba163d6b428c82e2ef7c7b2fa68087e24866b40
meenums26 <meenumsmav@gmail.com>
2020-08-17 15:01:37 +0530

    Updates

---
+++ precache-manifest.146005d6d25ff2d68f0f4c6b40e90e94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dabda641e80bd542606db3cc99fa3cd",
+    "url": "/daily-focus/index.html"
+  },
+  {
+    "revision": "8bcaf79aa39a69fda1dd",
+    "url": "/daily-focus/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b94916a49e498bd6c76c",
+    "url": "/daily-focus/static/css/main.7e3fd0ea.chunk.css"
+  },
+  {
+    "revision": "8bcaf79aa39a69fda1dd",
+    "url": "/daily-focus/static/js/2.58f373db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/daily-focus/static/js/2.58f373db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94916a49e498bd6c76c",
+    "url": "/daily-focus/static/js/main.7d335a69.chunk.js"
+  },
+  {
+    "revision": "9fce49284043d284e1b1",
+    "url": "/daily-focus/static/js/runtime-main.5ea814e9.js"
+  },
+  {
+    "revision": "c1f4fbc7f5be3fe9520a744eeb59d6fc",
+    "url": "/daily-focus/static/media/lerone-pieters-XnBrDrAJaoo-unsplash.c1f4fbc7.jpg"
+  },
+  {
+    "revision": "9c233d755ebd40caf33ea8bf6eb7a97f",
+    "url": "/daily-focus/static/media/newyork.9c233d75.png"
+  }
+]);
